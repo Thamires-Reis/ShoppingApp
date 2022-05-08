c22150ccb96b7c9c417737e8cebf194de3dd8e5b
Thamires-Reis <23763@student.dorset-college.ie>
2022-05-08 16:40:01 +0100

    Shopping App

---
+++ app/src/main/java/com/example/shoppingapp/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.shoppingapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity(), FragmentNavigation{
+    private lateinit var fAuth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        fAuth = Firebase.auth
+
+
+        supportFragmentManager.beginTransaction()
+            .add(R.id.container,LoginFragment())
+            .commit()
+
+
+    }
+
+    override fun navigateFrag(fragment: Fragment, addToStack: Boolean) {
+        val transaction = supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.container,fragment)
+        if (addToStack){
+           transaction.addToBackStack(null)
+        }
+        transaction.commit()
+    }
+}
---
+++ app/src/main/java/com/example/shoppingapp/Product.kt
@@ -0,0 +1,3 @@
+package com.example.shoppingapp
+
+class Product(var img: String, var title: String, var price: String)
